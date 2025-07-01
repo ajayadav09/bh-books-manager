1b842cff928210eee7a0f5c3e7d24c9f21334a82
Ajay Yadav <ajayadav09@gmail.com>
2025-07-01 18:23:57 +0530

    pushing to git

---
+++ books-manager.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Books Manager
+ * Description: A simple books manager plugin with custom post type and settings panel.
+ * Version: 1.0.0
+ * Author: Your Name
+ */
+
+defined('ABSPATH') || exit;
